5ee94e0d9640ae566f2ef4c8a597ad18169f1b35
Mabdelwanis <M.abdelwanis@live.com>
2023-01-16 19:47:33 +0200

    Update google-translate.js

--- jquery/js/google-translate.js
+++ jquery/js/google-translate.js
@@ -1,102 +1,54 @@
 /*!***************************************************
- * google-translate.js v1.0.4
+ * google-translate.js v1.0.0
  * https://Get-Web.Site/
- * author: Vitalii P.
+ * author: L2Banners
  *****************************************************/
 
 const googleTranslateConfig = {
-	/* Original language */
-	lang: "ru",
-
-	/* The language we translate into on the first visit*/
-	/* Язык, на который переводим при первом посещении */
-	/* langFirstVisit: 'en', */
-
-	/* Если скрипт не работает или работает неправильно, раскомментируйте и укажите основной домен в свойстве domain */
-	/* If the script does not work or does not work correctly, uncomment and specify the main domain in the domain property */
-	/* domain: "Get-Web.Site" */
+    lang: "ru",
 };
 
-$(function () {
-	/* Подключаем виджет google translate */
-	/* Connecting the google translate widget */
-	let script = document.createElement("script");
-	script.src = `//translate.google.com/translate_a/element.js?cb=TranslateWidgetIsLoaded`;
-	document.getElementsByTagName("head")[0].appendChild(script);
-});
+function TranslateInit() {
+    let code = TranslateGetCode();
+    // РќР°С…РѕРґРёРј С„Р»Р°Рі СЃ РІС‹Р±СЂР°РЅРЅС‹Рј СЏР·С‹РєРѕРј РґР»СЏ РїРµСЂРµРІРѕРґР° Рё РґРѕР±Р°РІР»СЏРµРј Рє РЅРµРјСѓ Р°РєС‚РёРІРЅС‹Р№ РєР»Р°СЃСЃ
+    $('[data-google-lang="' + code + '"]').addClass('language__img_active');
 
-function TranslateWidgetIsLoaded() {
-	TranslateInit(googleTranslateConfig);
+    if (code == googleTranslateConfig.lang) {
+        // Р•СЃР»Рё СЏР·С‹Рє РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ, СЃРѕРІРїР°РґР°РµС‚ СЃ СЏР·С‹РєРѕРј РЅР° РєРѕС‚РѕСЂС‹Р№ РїРµСЂРµРІРѕРґРёРј
+        // РўРѕ РѕС‡РёС‰Р°РµРј РєСѓРєРё
+        TranslateClearCookie();
+    }
+
+    // РРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј РІРёРґР¶РµС‚ СЃ СЏР·С‹РєРѕРј РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+    new google.translate.TranslateElement({
+        pageLanguage: googleTranslateConfig.lang,
+    });
+
+    // Р’РµС€Р°РµРј СЃРѕР±С‹С‚РёРµ  РєР»РёРє РЅР° С„Р»Р°РіРё
+    $('[data-google-lang]').click(function () {
+        TranslateSetCookie($(this).attr("data-google-lang"))
+        // РџРµСЂРµР·Р°РіСЂСѓР¶Р°РµРј СЃС‚СЂР°РЅРёС†Сѓ
+        window.location.reload();
+    });
 }
 
-function TranslateInit(config) {
-	if (config.langFirstVisit && !$.cookie("googtrans")) {
-		/* Если установлен язык перевода для первого посещения и куки не назначены */
-		/* If the translation language is installed for the first visit and cookies are not assigned */
-		TranslateCookieHandler("/auto/" + config.langFirstVisit);
-	}
-
-	let code = TranslateGetCode(config);
-
-	TranslateHtmlHandler(code);
-
-	if (code == config.lang) {
-		/* Если язык по умолчанию, совпадает с языком на который переводим, то очищаем куки */
-		/* If the default language is the same as the language we are translating into, then we clear the cookies */
-		TranslateCookieHandler(null, config.domain);
-	}
-
-	/* Инициализируем виджет с языком по умолчанию */
-	/* Initialize the widget with the default language */
-	new google.translate.TranslateElement({
-		pageLanguage: config.lang,
-	});
-
-	/* Вешаем событие  клик на флаги */
-	/* Assigning a handler to the flags */
-	$("[data-google-lang]").click(function () {
-		TranslateCookieHandler(
-			"/auto/" + $(this).attr("data-google-lang"),
-			config.domain
-		);
-		/* Перезагружаем страницу */
-		/* Reloading the page */
-		window.location.reload();
-	});
+function TranslateGetCode() {
+    // Р•СЃР»Рё РєСѓРєРё РЅРµС‚, С‚Рѕ РїРµСЂРµРґР°РµРј РґРµС„РѕР»С‚РЅС‹Р№ СЏР·С‹Рє
+    let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
+    return lang.substr(-2);
 }
 
-function TranslateGetCode(config) {
-	/* Если куки нет, то передаем дефолтный язык */
-	/* If there are no cookies, then we pass the default language */
-	let lang =
-		$.cookie("googtrans") != undefined && $.cookie("googtrans") != "null"
-			? $.cookie("googtrans")
-			: config.lang;
-	return lang.match(/(?!^\/)[^\/]*$/gm)[0];
+function TranslateClearCookie() {
+    $.cookie('googtrans', null);
+    $.cookie("googtrans", null, {
+        domain: "." + document.domain,
+    });
 }
 
-function TranslateCookieHandler(val, domain) {
-	/* Записываем куки /язык_который_переводим/язык_на_который_переводим */
-	/* Writing down cookies /language_for_translation/the_language_we_are_translating_into */
-	$.cookie("googtrans", val);
-	$.cookie("googtrans", val, {
-		domain: "." + document.domain,
-	});
-
-	if (domain == "undefined") return;
-	/* записываем куки для домена, если он назначен в конфиге */
-	/* Writing down cookies for the domain, if it is assigned in the config */
-	$.cookie("googtrans", val, {
-		domain: domain,
-	});
-
-	$.cookie("googtrans", val, {
-		domain: "." + domain,
-	});
-}
-
-function TranslateHtmlHandler(code) {
-	/* Получаем язык на который переводим и производим необходимые манипуляции с DOM */
-	/* We get the language to which we translate and produce the necessary manipulations with DOM */
-	$('[data-google-lang="' + code + '"]').addClass("language__img_active");
+function TranslateSetCookie(code) {
+    // Р—Р°РїРёСЃС‹РІР°РµРј РєСѓРєРё /СЏР·С‹Рє_РєРѕС‚РѕСЂС‹Р№_РїРµСЂРµРІРѕРґРёРј/СЏР·С‹Рє_РЅР°_РєРѕС‚РѕСЂС‹Р№_РїРµСЂРµРІРѕРґРёРј
+    $.cookie('googtrans', "/auto/" + code);
+    $.cookie("googtrans", "/auto/" + code, {
+        domain: "." + document.domain,
+    });
 }
